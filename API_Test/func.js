af5e981d6467a92640e5b81b53cc837bd03c0407
yusuke <34528586+yunTum@users.noreply.github.com>
2020-09-04 14:43:39 +0900

    APItest_update
    view cards

--- API_Test/func.js
+++ API_Test/func.js
@@ -1,4 +1,18 @@
 var URL
+
+temp_top ="<div class='card'>"
+temp_img_front = "<img id='image_place-tag' src="
+var img_src
+temp_img_back = " width='60%' height='30%'/>"
+temp_place_front = "<p id='place-tag'>"
+var place_tag
+temp_price_front = "<p id='price-tag'>"
+var price_tag
+temp_explain_front = "<p id='explain-tag'>"
+var explain_tag
+temp_back = "</p>"
+temp_bottom = "</div>"
+
 function getAPI(){
     apiURL =  'http://localhost:8000/' + URL
     var request = new XMLHttpRequest();
@@ -7,15 +21,41 @@ function getAPI(){
 
     request.onload = function () {
         var data = this.response
-
         document.getElementById("apidata").innerText = JSON.stringify(data);
+    }
 
-        if (data[0]["photo"]){
-            document.getElementById("image_place").src = data[0]["photo"]
+    request.send();
+}
+
+function getCards(){
+    apiURL =  'http://localhost:8000/api/cards/'
+    var request = new XMLHttpRequest();
+    request.open('GET', apiURL, true);
+    request.responseType = 'json';
+
+    request.onload = function () {
+        var data = this.response
+        console.log(data.length)
+        for(var i=0; i<data.length; i++){
+            console.log(i)
+            document.getElementById("cards-area").innerHTML += temp_top
+                                                            + temp_img_front
+                                                            + data[i].photo
+                                                            + temp_img_back
+                                                            + temp_place_front
+                                                            + '場所：' + data[i].place
+                                                            + temp_back
+                                                            + temp_price_front
+                                                            + '値段：' + data[i].price + '　特徴：' + data[i].price
+                                                            + temp_back
+                                                            + temp_explain_front
+                                                            + data[i].explain
+                                                            + temp_back
+                                                            + temp_bottom
         }
-        else{
-            document.getElementById("image_place").src = ""
-        }
+        //    document.getElementById("image_place-tag").src = data[0]["photo"]
+        var content = document.getElementById("cards-area").innerHTML
+        console.log(content)
     }
 
     request.send();
